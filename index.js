213fb01d47c696ac1d8bb2783fd994c6ceeaf1a1
Mohammad Shahnawaz <shahnawaz919956@gmail.com>
2024-02-07 14:08:27 +0530

    added files

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello")
+// import { countryList } from "./data";
+
+const BASE_URL =
+"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+
+
+// api = https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json
