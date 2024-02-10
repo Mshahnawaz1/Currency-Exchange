727c99b639489c51eb26702eb9d85225c7a19aaa
Mohammad Shahnawaz <shahnawaz919956@gmail.com>
2024-02-10 16:28:18 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,3 @@
-// api = https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json
-
 
 const BASE_URL =
 "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
