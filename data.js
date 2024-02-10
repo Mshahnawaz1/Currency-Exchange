240dae57d34916d213ff1d5d188a57c982ce44f7
Mohammad Shahnawaz <shahnawaz919956@gmail.com>
2024-02-10 16:13:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,189 +1,59 @@
-const countryList = {
-    AED: "AE",
-    AFN: "AF",
-    XCD: "AG",
-    ALL: "AL",
-    AMD: "AM",
-    ANG: "AN",
-    AOA: "AO",
-    AQD: "AQ",
-    ARS: "AR",
-    AUD: "AU",
-    AZN: "AZ",
-    BAM: "BA",
-    BBD: "BB",
-    BDT: "BD",
-    XOF: "BE",
-    BGN: "BG",
-    BHD: "BH",
-    BIF: "BI",
-    BMD: "BM",
-    BND: "BN",
-    BOB: "BO",
-    BRL: "BR",
-    BSD: "BS",
-    NOK: "BV",
-    BWP: "BW",
-    BYR: "BY",
-    BZD: "BZ",
-    CAD: "CA",
-    CDF: "CD",
-    XAF: "CF",
-    CHF: "CH",
-    CLP: "CL",
-    CNY: "CN",
-    COP: "CO",
-    CRC: "CR",
-    CUP: "CU",
-    CVE: "CV",
-    CYP: "CY",
-    CZK: "CZ",
-    DJF: "DJ",
-    DKK: "DK",
-    DOP: "DO",
-    DZD: "DZ",
-    ECS: "EC",
-    EEK: "EE",
-    EGP: "EG",
-    ETB: "ET",
-    EUR: "FR",
-    FJD: "FJ",
-    FKP: "FK",
-    GBP: "GB",
-    GEL: "GE",
-    GGP: "GG",
-    GHS: "GH",
-    GIP: "GI",
-    GMD: "GM",
-    GNF: "GN",
-    GTQ: "GT",
-    GYD: "GY",
-    HKD: "HK",
-    HNL: "HN",
-    HRK: "HR",
-    HTG: "HT",
-    HUF: "HU",
-    IDR: "ID",
-    ILS: "IL",
-    INR: "IN",
-    IQD: "IQ",
-    IRR: "IR",
-    ISK: "IS",
-    JMD: "JM",
-    JOD: "JO",
-    JPY: "JP",
-    KES: "KE",
-    KGS: "KG",
-    KHR: "KH",
-    KMF: "KM",
-    KPW: "KP",
-    KRW: "KR",
-    KWD: "KW",
-    KYD: "KY",
-    KZT: "KZ",
-    LAK: "LA",
-    LBP: "LB",
-    LKR: "LK",
-    LRD: "LR",
-    LSL: "LS",
-    LTL: "LT",
-    LVL: "LV",
-    LYD: "LY",
-    MAD: "MA",
-    MDL: "MD",
-    MGA: "MG",
-    MKD: "MK",
-    MMK: "MM",
-    MNT: "MN",
-    MOP: "MO",
-    MRO: "MR",
-    MTL: "MT",
-    MUR: "MU",
-    MVR: "MV",
-    MWK: "MW",
-    MXN: "MX",
-    MYR: "MY",
-    MZN: "MZ",
-    NAD: "NA",
-    XPF: "NC",
-    NGN: "NG",
-    NIO: "NI",
-    NPR: "NP",
-    NZD: "NZ",
-    OMR: "OM",
-    PAB: "PA",
-    PEN: "PE",
-    PGK: "PG",
-    PHP: "PH",
-    PKR: "PK",
-    PLN: "PL",
-    PYG: "PY",
-    QAR: "QA",
-    RON: "RO",
-    RSD: "RS",
-    RUB: "RU",
-    RWF: "RW",
-    SAR: "SA",
-    SBD: "SB",
-    SCR: "SC",
-    SDG: "SD",
-    SEK: "SE",
-    SGD: "SG",
-    SKK: "SK",
-    SLL: "SL",
-    SOS: "SO",
-    SRD: "SR",
-    STD: "ST",
-    SVC: "SV",
-    SYP: "SY",
-    SZL: "SZ",
-    THB: "TH",
-    TJS: "TJ",
-    TMT: "TM",
-    TND: "TN",
-    TOP: "TO",
-    TRY: "TR",
-    TTD: "TT",
-    TWD: "TW",
-    TZS: "TZ",
-    UAH: "UA",
-    UGX: "UG",
-    USD: "US",
-    UYU: "UY",
-    UZS: "UZ",
-    VEF: "VE",
-    VND: "VN",
-    VUV: "VU",
-    YER: "YE",
-    ZAR: "ZA",
-    ZMK: "ZM",
-    ZWD: "ZW",
-  };
+// api = https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json
 
-console.log(dropdown);
+
+const BASE_URL =
+"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdown = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select")
 const toCurr = document.querySelector(".to select")
 const msg = document.querySelector(".msg")
+const reverse = document.querySelector(".reverse")
 
-console.log("count")
-for (let select of dropdown){
-  for (r in countryList){
-    // console.log(r, countryList[r]);
-    let newOption = document.createElement("option");
-    newOption.innerText = r;
-    if(select.name === "from" && r === "USD"){
-        newOption.selected = "selected"
+document.addEventListener('DOMContentLoaded', () => {
+    updateExchange();
+    fillOptions();
+
+    // calculate the value for selected Curr
+    btn.addEventListener('click' , (evt) =>{
+        evt.preventDefault();
+        updateExchange()    
+    })
+    
+    // reverse the exchange countries
+    reverse.addEventListener('click',() =>{
+        let tmp = fromCurr.value
+        fromCurr.value = toCurr.value
+        toCurr.value = tmp
+
+        updateFlag(fromCurr)
+        updateFlag(toCurr)
+    })
+})
+
+function fillOptions(){
+    for (let select of dropdown){
+      for (r in countryList){
+        // console.log(r, countryList[r]);
+        let newOption = document.createElement("option");
+        // newOption.innerHTML = `<p>${r} <img class="option-flag" src="https://flagsapi.com/${countryList[r]}/flat/64.png"> </p>`
+        newOption.innerText = r;
+        
+        if(select.name === "from" && r === "USD"){
+            newOption.selected = "selected"
+        }
+        else if (select.name === "to" && r === "INR"){
+            newOption.selected = "selected"
+        } 
+        select.append(newOption)
+      }
+        select.addEventListener("change", (evt) => {
+            updateFlag(evt.target)
+        });
     }
-    else if (select.name === "to" && r === "INR"){
-        newOption.selected = "selected"
-    } 
-    select.append(newOption)
-  }
-    select.addEventListener("change", (evt) => {
-        updateFlag(evt.target)
-    });
 }
+
 const updateFlag = (element) => {
     let currCode = element.value;
     console.log(currCode,countryList[currCode])
@@ -192,14 +62,8 @@ const updateFlag = (element) => {
     img.src = newlink;
 }
 
-btn.addEventListener('click' , (evt) =>{
-    evt.preventDefault();
-    updateExchange()
-    
-})
-window.addEventListener('load' , ()=> {
-    updateExchange();
-})
+
+// update the exchange value
 const updateExchange =async ()=>{
     let amount = document.querySelector(".amount input");
     let amt = amount.value;
